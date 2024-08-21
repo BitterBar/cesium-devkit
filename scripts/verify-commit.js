637ce69756913153a04c6ded6efc4fd5756a9668
袁照阳 <alwaysmissly@163.com>
2024-08-21 16:18:02 +0800

    chore(verify-commit): add verify-commit comment to commit message

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -6,6 +6,24 @@ import path from 'node:path'
 const msgPath = path.resolve('.git/COMMIT_EDITMSG')
 const msg = readFileSync(msgPath, 'utf-8').trim()
 
+/**
+ * feat：新功能
+ * fix：修复问题
+ * docs：文档变更
+ * dx：开发体验改进
+ * style：代码格式（不影响代码运行的变动）
+ * refactor：重构（既不是新增功能也不是修复 bug 的代码变动）
+ * perf：性能优化
+ * test：添加测试
+ * workflow：工作流相关变动
+ * build：构建系统或外部依赖的变动
+ * ci：持续集成相关变动
+ * chore：其他不修改 src 或测试文件的变动
+ * types：类型定义文件的变动
+ * wip：进行中的工作
+ * release：发布新版本
+ */
+
 const commitRE =
   /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
 
